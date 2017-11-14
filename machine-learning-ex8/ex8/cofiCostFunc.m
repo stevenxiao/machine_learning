49de392e224c8311faeff869ccdd0caedb8e6fbb
Steven Xiao <xiaoxi328@gmail.com>
2017-11-15 00:01:20 +0800

    finish ex8

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -11,13 +11,6 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-%disp(X);
-%disp("\n");
-%disp(Theta);
-%disp("\n");
-%disp(Y);
-%disp("\n");
-%disp(R);
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -27,7 +20,7 @@ Theta_grad = zeros(size(Theta));
 % Instructions: Compute the cost function and gradient for collaborative
 %               filtering. Concretely, you should first implement the cost
 %               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
+%               matches our costs. After that, you should implement the
 %               gradient and use the checkCostFunction routine to check
 %               that the gradient is correct. Finally, you should implement
 %               regularization.
@@ -35,14 +28,14 @@ Theta_grad = zeros(size(Theta));
 % Notes: X - num_movies  x num_features matrix of movie features
 %        Theta - num_users  x num_features matrix of user features
 %        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
+%        R - num_movies x num_users matrix, where R(i, j) = 1 if the
 %            i-th movie was rated by the j-th user
 %
 % You should set the following variables correctly:
 %
-%        X_grad - num_movies x num_features matrix, containing the 
+%        X_grad - num_movies x num_features matrix, containing the
 %                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
+%        Theta_grad - num_users x num_features matrix, containing the
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
@@ -54,14 +47,36 @@ for i = 1:num_movies
     end
 end
 
+theta2 = 0;
+
+for i = 1:num_users
+  for j = 1:num_features
+    theta2 += lambda * (Theta(i, j)^2)/2;
+  end
+end
+x2 = 0;
+for i = 1: num_movies
+  for j = 1:num_features
+    x2 += lambda*(X(i, j)^2)/2;
+  end
+end
+
+J += (theta2 + x2);
 
 for i = 1:num_movies
     idx = find(R(i ,:) == 1);
     Theta_tmp = Theta(idx,:);
     Y_tmp = Y(i,idx);
-    X_grad(i,:) = (X(i,:)*Theta_tmp' - Y_tmp) * Theta_tmp;
+    X_grad(i,:) = (X(i,:)*Theta_tmp' - Y_tmp) * Theta_tmp + lambda * X(i,:);
 end
- 
+
+for j = 1: num_users
+   idx = find(R(:, j) == 1);
+   X_tmp = X(idx,:);
+   Y_tmp = Y(idx, j);
+   Theta_grad(j,:) = (X_tmp * Theta(j,:)' - Y_tmp)' * X_tmp + lambda * Theta(j,:);
+end
+
 %for i = 1:num_movies
 %    for j = 1:num_users
 %        if R(i, j) == 1
